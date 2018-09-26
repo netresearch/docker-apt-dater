9b72490a8f06a7d7f7f6a6364765db2320799a29
Sebastian Mendel <sebastian.mendel@netresearch.de>
2018-09-26 13:07:32 +0200

    allow passing and executing other commands than just apt-dater arguments - enables f. e. executing bash in container

--- setup/entrypoint.sh
+++ setup/entrypoint.sh
@@ -4,4 +4,16 @@ set -ex
 cat /opt/apt-dater/hosts.d.xml.head /opt/apt-dater/hosts.d/*.xml /opt/apt-dater/hosts.d.xml.foot > /opt/apt-dater/hosts.d.xml
 cat /opt/apt-dater/hosts.d.xml
 
-exec apt-dater -c /opt/apt-dater/apt-dater.xml "$@"
+
+# first arg is `-f` or `--some-option` - set apt-dater as entrypoint command and add provided arguments
+if [ "${1#-}" != "$1" ]; then
+    set -- apt-dater -c /opt/apt-dater/apt-dater.xml "$@"
+fi
+
+# no arguments at all - set apt-dater as entry point command
+if [ $# -eq 0 ]; then
+    set -- apt-dater -c /opt/apt-dater/apt-dater.xml "$@"
+fi
+
+
+exec "$@"
